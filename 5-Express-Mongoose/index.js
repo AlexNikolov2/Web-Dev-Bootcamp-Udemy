d55a53379fe59970f7cb7e0256e8c9396f70061a
Aleks Nikolov <tcr.official.world@gmail.com>
2024-09-02 12:13:39 +0300

    opened mongo connection

--- 5-Express-Mongoose/index.js
+++ 5-Express-Mongoose/index.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+mongoose
+  .connect("mongodb://localhost:27017/testApp")
+  .then(() => {
+    console.log("OPEN CONNECTION!");
+  })
+  .catch((err) => {
+    console.log("FAILED!");
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Welcome");
