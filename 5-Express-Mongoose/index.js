90cd69878c974f8eb6683e26278f8090d4d19574
Aleks Nikolov <tcr.official.world@gmail.com>
2024-09-02 12:00:39 +0300

    training

---
+++ 5-Express-Mongoose/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.get("/dog", (req, res) => {
+  res.send("WOOF");
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on 3000");
+});
