9ade39efc30b962f0716838f32985030510b8516
Aleks Nikolov <tcr.official.world@gmail.com>
2024-09-21 00:52:33 +0300

    created header

---
+++ know-your-capital/front/src/components/Header.jsx
@@ -0,0 +1,22 @@
+export const HeaderComponent = () => {
+  return (
+    <header>
+      <div className="left">
+        <h2>Know The Capital</h2>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Play</a>
+          </li>
+          <li>
+            <a href="">Learn</a>
+          </li>
+          <li>
+            <a href="">User</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
