f51d3a7f49335b109f26162fd52fa75e8a7764b9
Aleks Nikolov <tcr.official.world@gmail.com>
2024-09-24 22:08:54 +0300

    create functionality

--- 5-Express-Mongoose/index.js
+++ 5-Express-Mongoose/index.js
@@ -7,6 +7,7 @@ const Product = require("./models/product");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded());
 
 mongoose
   .connect("mongodb://localhost:27017/farmApp")
@@ -26,6 +27,19 @@ app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("products/index", { products });
 });
+
+app.get("/products/create", (req, res) => {
+  res.render("products/create");
+})
+
+app.post("/products", async(req, res) => {
+  console.log(req.body);
+  const newProduct = new Product(req.body);
+
+  
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+})
 
 app.get("/products/:id", async (req, res) => {
   const id = req.params.id;
