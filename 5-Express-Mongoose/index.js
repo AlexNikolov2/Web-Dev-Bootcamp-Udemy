d3316b526ce0e035b916a0dcd4414c366865a8c8
Aleks Nikolov <tcr.official.world@gmail.com>
2024-10-01 21:58:08 +0300

    i fixed it all, forgot method override

--- 5-Express-Mongoose/index.js
+++ 5-Express-Mongoose/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
 const path = require("path");
 const mongoose = require("mongoose");
 
@@ -8,6 +9,7 @@ const Product = require("./models/product");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
+app.use(methodOverride('_method'));
 
 mongoose
   .connect("mongodb://localhost:27017/farmApp")
@@ -54,15 +56,15 @@ app.get("/products/:id/edit", async (req, res) => {
   res.render('products/update', {product});
 })
 
-app.put("/products/:id/edit", async (req, res) => {
-  const newProduct = new Product(req.body);
-  
-  await newProduct.save();
-  res.redirect(`/products/${newProduct._id}`);
+app.put('/products/:id', async (req, res) => {
+  const id = req.params.id;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+  res.redirect(`/products/${product._id}`);
+
 })
 
 
-app.delete('/delete/:id', async (req, res) => {
+app.delete('/products/:id', async (req, res) => {
   const { id } = req.params;
   await Product.findByIdAndDelete(id);
   res.redirect('/products');
