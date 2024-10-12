9e9776f628d5ce99efabe4503de46088dd010fd7
Aleks Nikolov <tcr.official.world@gmail.com>
2024-10-13 00:37:00 +0300

    register skeleton

--- know-your-capital/front/src/pages/auth/Register/Register.jsx
+++ know-your-capital/front/src/pages/auth/Register/Register.jsx
@@ -1,3 +1,22 @@
 export function Register() {
-  <section className="form register"></section>;
+  return (
+    <section className="form register">
+      <form action="">
+        <h2>Register</h2>
+        <section className="field username">
+          <label htmlFor="username">Username</label>
+          <input type="text" placeholder="Enter your username..." />
+        </section>
+        <section className="field username">
+          <label htmlFor="username">Email</label>
+          <input type="text" placeholder="Enter your email..." />
+        </section>
+        <section className="field password">
+          <label htmlFor="password">Pasword</label>
+          <input type="password" placeholder="Enter your password" />
+        </section>
+        <button>Sign up!</button>
+      </form>
+    </section>
+  );
 }
