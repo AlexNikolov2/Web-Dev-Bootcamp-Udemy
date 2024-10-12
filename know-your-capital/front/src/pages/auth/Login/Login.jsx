8b66a1b3846cc63104740c7850dc7843c712a5d2
Aleks Nikolov <tcr.official.world@gmail.com>
2024-10-13 00:33:46 +0300

    html skeleton for login component

--- know-your-capital/front/src/pages/auth/Login/Login.jsx
+++ know-your-capital/front/src/pages/auth/Login/Login.jsx
@@ -1,3 +1,17 @@
 export function Login() {
-  return <section className="form login"></section>;
+  return (
+    <section className="form login">
+      <form action="">
+        <h2>Welcome back</h2>
+        <section className="field username">
+          <label htmlFor="username">Username</label>
+          <input type="text" placeholder="Enter your username..." />
+        </section>
+        <section className="field password">
+          <label htmlFor="password">Pasword</label>
+          <input type="password" placeholder="Enter your password" />
+        </section>
+      </form>
+    </section>
+  );
 }
