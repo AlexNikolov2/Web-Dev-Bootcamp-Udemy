5b9e4dc62317af4b1aaec81f961d71d8a2568c51
Aleks Nikolov <tcr.official.world@gmail.com>
2024-10-26 00:41:58 +0300

    login

--- know-your-capital/front/src/pages/auth/Login/Login.jsx
+++ know-your-capital/front/src/pages/auth/Login/Login.jsx
@@ -1,15 +1,25 @@
+import TextField from "@mui/material/TextField";
+
 export function Login() {
   return (
     <section className="form login">
       <form action="">
         <h2>Welcome back</h2>
         <section className="field username">
-          <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Enter your username..." />
+          <TextField
+            id="standard-basic"
+            label="Username"
+            variant="standard"
+            fullWidth
+          />
         </section>
         <section className="field password">
-          <label htmlFor="password">Pasword</label>
-          <input type="password" placeholder="Enter your password" />
+          <TextField
+            id="standard-basic"
+            label="Password"
+            variant="standard"
+            fullWidth
+          />
         </section>
         <button>Log in!</button>
       </form>
