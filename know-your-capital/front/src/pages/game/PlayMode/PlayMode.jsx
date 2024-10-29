379cebacf42fe01acc978f31cb3d5d39ccd42312
Aleks Nikolov <tcr.official.world@gmail.com>
2024-10-29 16:38:39 +0200

    .

--- know-your-capital/front/src/pages/game/PlayMode/PlayMode.jsx
+++ know-your-capital/front/src/pages/game/PlayMode/PlayMode.jsx
@@ -1,9 +1,11 @@
+import "./PlayMode.css";
+
 export function PlayMode() {
   return (
     <section>
       <h2>Play Mode - Guess All Capitals</h2>
       <p>Start guessing the capitals of all countries!</p>
-      {/* Add more content for the Play Mode page */}
+      <button>Start</button>
     </section>
   );
 }
