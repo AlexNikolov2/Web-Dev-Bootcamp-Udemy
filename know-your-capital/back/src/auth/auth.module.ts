742847b86331aa4b7b00104ffe80b4d0e5e96b9a
Aleks Nikolov <tcr.official.world@gmail.com>
2024-11-26 14:30:39 +0200

    testing lazy modules D

--- know-your-capital/back/src/auth/auth.module.ts
+++ know-your-capital/back/src/auth/auth.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 
 @Module({
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
+@Injectable()
 export class AuthModule {}
--- know-your-capital/back/src/auth/auth/auth.service.ts
+++ know-your-capital/back/src/auth/auth/auth.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { LazyModuleLoader } from '@nestjs/core/injector/lazy-module-loader/lazy-module-loader';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private lazyModuleLoader: LazyModuleLoader) {}
+}
