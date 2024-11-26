cef15754c59b7e1f20000fe6cc1b79cb626734a8
Aleks Nikolov <tcr.official.world@gmail.com>
2024-11-26 14:25:20 +0200

    generating controller and service for game

--- know-your-capital/back/src/game/game.module.ts
+++ know-your-capital/back/src/game/game.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { GameService } from './game/game.service';
+import { GameController } from './game/game.controller';
 
-@Module({})
+@Module({
+  providers: [GameService],
+  controllers: [GameController]
+})
 export class GameModule {}
