4a6195325712d0cfc6724060f66cf95d01735196
Aleks Nikolov <tcr.official.world@gmail.com>
2024-11-26 14:21:36 +0200

    generating required service and controllers

--- know-your-capital/back/src/auth/auth.module.ts
+++ know-your-capital/back/src/auth/auth.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
-@Module({})
+@Module({
+  providers: [AuthService],
+  controllers: [AuthController]
+})
 export class AuthModule {}
