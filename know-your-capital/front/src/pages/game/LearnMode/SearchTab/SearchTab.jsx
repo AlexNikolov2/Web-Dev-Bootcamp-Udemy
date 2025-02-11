95debf595d9c313d311e5030736df04d3666384b
Aleks Nikolov <tcr.official.world@gmail.com>
2025-02-11 21:59:17 +0200

    made textFieldStyling exportable, optimizing it for other needed places

--- know-your-capital/front/src/pages/game/LearnMode/SearchTab/SearchTab.jsx
+++ know-your-capital/front/src/pages/game/LearnMode/SearchTab/SearchTab.jsx
@@ -1,5 +1,11 @@
 export function SearchTab() {
     return (
-        <div>Search Tab Works!</div>
+        <section>
+            <section className="title">
+                <h2>Search & Learn</h2>
+                <p>Find information about every capital Of each country in the world!</p>
+            </section>
+
+        </section>
     );
 }
