7232d7ce1737b94364ac5a66d33aceb709638c5a
Aleks Nikolov <tcr.official.world@gmail.com>
2025-02-20 21:58:50 +0200

    3 - implementing html structure

--- know-your-capital/front/src/pages/game/LearnMode/Capital/Capital.jsx
+++ know-your-capital/front/src/pages/game/LearnMode/Capital/Capital.jsx
@@ -1,7 +1,26 @@
+import image from '../../../../assets/syria.webp'
+
 export function Capital() {
+    const population = 6500000;
+    const yearFounded = 1390;
     return (
         <section className="capital-wrap">
-            <p>Capital works!</p>
+            <section className="container-left">
+                <img src={image} alt="" />
+                <p>Population : {population} </p>
+                <p>Founded in : {yearFounded}</p>
+            </section>
+            <section className="container-right">
+                <section className="right-title">
+                    <h2>Dimashq</h2>
+                    <h3>Capital of Souria</h3>
+                </section>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem rem quae,
+                    omnis vel nulla impedit qui voluptates libero, itaque ab labore eos velit unde.
+                    Dolore neque deserunt ipsum ea numquam!
+                </p>
+            </section>
         </section>
     );
 }
