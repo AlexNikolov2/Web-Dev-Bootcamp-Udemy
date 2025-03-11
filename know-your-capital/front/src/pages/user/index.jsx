0cea806299ebe1e3ca2a0ed3faa9000beca2b717
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-11 23:20:59 +0200

    19 - defined html stucture

--- know-your-capital/front/src/pages/user/index.jsx
+++ know-your-capital/front/src/pages/user/index.jsx
@@ -1,5 +1,23 @@
 import './index.css';
 
 export function User() {
-    return <>user works!</>
+    return (
+        <section className="user-wrap">
+            <section className="user-title-wrap">
+                <h2>Your Profile</h2>
+            </section>
+            <section className="games-summary-wrap">
+                tralala
+            </section>
+            <section className="buttons-wrap">
+                <button className="edit-profile-btn">
+                    Edit Profile
+                </button>
+                <button className="change-plan-btn">
+                    Change plan
+                </button>
+            </section>
+        </section>
+    )
+
 }
