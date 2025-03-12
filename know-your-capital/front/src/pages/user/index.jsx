b9a4ad5ccfeb3686cb5d2c08aadcf76146855304
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-12 22:51:57 +0200

    completed html stucture

--- know-your-capital/front/src/pages/user/index.jsx
+++ know-your-capital/front/src/pages/user/index.jsx
@@ -7,7 +7,21 @@ export function User() {
                 <h2>Your Profile</h2>
             </section>
             <section className="games-summary-wrap">
-                tralala
+                <p>Your latest game</p>
+                <section className="latest-game">
+                    <section className="lg-result">
+                        <p>Result</p>
+                        <p className="stat">
+                            188/195
+                        </p>
+                    </section>
+                    <section className="lg-time">
+                        <p>Time</p>
+                        <p className="stat">
+                            17:44
+                        </p>
+                    </section>
+                </section>
             </section>
             <section className="buttons-wrap">
                 <button className="edit-profile-btn">
