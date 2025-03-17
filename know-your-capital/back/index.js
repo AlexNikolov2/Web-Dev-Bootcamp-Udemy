059e41600c5ae87761bcf198e420e0a5245e4580
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-17 20:57:13 +0200

    installing path lib

--- know-your-capital/back/index.js
+++ know-your-capital/back/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get()
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 })
