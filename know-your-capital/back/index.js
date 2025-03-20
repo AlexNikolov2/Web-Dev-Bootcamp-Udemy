2fa76461c57f61419c86d07cf3fbca4a0c6cd9c7
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-20 23:22:36 +0200

    how tf i missed the cors

--- know-your-capital/back/index.js
+++ know-your-capital/back/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 const authController = require('./controllers/authController');
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../front/dist')));
-
-app.get('/api/example', (req, res) => {
-    res.json({ message: 'Hello from the server!' });
-});
 
 app.use('/auth', authController);
 
