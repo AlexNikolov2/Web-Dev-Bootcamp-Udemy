5826300bf1aa9f92e6f2da7bb9311ef76bc28820
Aleks Nikolov <tcr.official.world@gmail.com>
2025-03-20 22:52:03 +0200

    app now uses the app controller

--- know-your-capital/back/index.js
+++ know-your-capital/back/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const authController = require('./controllers/authController');
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../front/dist')));
@@ -8,6 +9,8 @@ app.use(express.static(path.join(__dirname, '../front/dist')));
 app.get('/api/example', (req, res) => {
     res.json({ message: 'Hello from the server!' });
 });
+
+app.use('/auth', authController);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../front/dist', 'index.html'));
