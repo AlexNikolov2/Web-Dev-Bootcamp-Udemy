7f05fa3ffcb1546b2b87829b708620e05f4c6957
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-08 00:10:55 +0300

    removed useless route

--- know-your-capital/back/controllers/gameController.js
+++ know-your-capital/back/controllers/gameController.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const fs = require("fs");
+const path = require("path");
 const Country = require("../models/Country");
 
 const router = Router();
@@ -25,23 +27,17 @@ router.get("/game/learn-mode/:id", async (req, res) => {
   res.json(learnCountry);
 });
 
-
-router.get("/game/play-mode", async (req, res) => {
-  const playCountries = await Country.find().populate(
-    "countries"
-  );
-
-  res.json(playCountries);
-
-})
-
 router.get("/game/play-mode/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const playCountry = await Country.findOne({ _id: id }); // Ensure the query matches the database schema
+    const dataPath = path.join(__dirname, "../data/data.json");
+    const data = JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+    const playCountry = data.find((country) => country.id === parseInt(id));
+
     if (!playCountry) {
       return res.status(404).json({ error: "Country not found" });
     }
+
     res.json(playCountry);
   } catch (error) {
     console.error("Error fetching country:", error);
