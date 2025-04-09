109d31151fbe65d0d73afceec421e88ed8e8bc96
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-10 00:58:21 +0300

    adding searchTab as main Learn Mode

--- know-your-capital/front/src/pages/game/LearnMode/index.jsx
+++ know-your-capital/front/src/pages/game/LearnMode/index.jsx
@@ -1,28 +1,37 @@
+import { useState } from "react";
 import { TextField } from "@mui/material";
 import { textFieldStyling } from "../../../utils/textfield_styling";
 import "./style.css";
 
 export function LearnMode() {
+  const [searched, setSearched] = useState(false);
+
+  const handleSearch = () => {
+    setSearched(true);
+  };
+
   return (
-    <section className="search-wrap">
-      <section className="title">
-        <h2>Search & Learn</h2>
-        <p>
-          Find information about every capital Of each country in the world!
-        </p>
+    searched && (
+      <section className="search-wrap">
+        <section className="title">
+          <h2>Search & Learn</h2>
+          <p>
+            Find information about every capital Of each country in the world!
+          </p>
+        </section>
+        <section className="search">
+          <TextField
+            id="standard-basic"
+            label="Type a country, or a country’s capital"
+            variant="standard"
+            sx={textFieldStyling}
+            fullWidth
+          />
+          <button className="search-btn" onClick={handleSearch()}>
+            Search
+          </button>
+        </section>
       </section>
-      <section className="search">
-        <TextField
-          id="standard-basic"
-          label="Type a country, or a country’s capital"
-          variant="standard"
-          sx={textFieldStyling}
-          fullWidth
-        />
-        <button className="search-btn" onClick>
-          Search
-        </button>
-      </section>
-    </section>
+    )
   );
 }
