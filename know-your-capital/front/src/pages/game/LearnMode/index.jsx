1c1f257510af81803ef7ef7d3d32b70f04a1b1ec
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-10 00:56:00 +0300

    removed search tab file

--- know-your-capital/front/src/pages/game/LearnMode/index.jsx
+++ know-your-capital/front/src/pages/game/LearnMode/index.jsx
@@ -1,8 +1,28 @@
-import { Capital } from "./Capital";
-import { SearchTab } from "./SearchTab";
+import { TextField } from "@mui/material";
+import { textFieldStyling } from "../../../utils/textfield_styling";
+import "./style.css";
 
 export function LearnMode() {
-  let isSearched = true;
-
-  return isSearched ? <Capital /> : <SearchTab />;
+  return (
+    <section className="search-wrap">
+      <section className="title">
+        <h2>Search & Learn</h2>
+        <p>
+          Find information about every capital Of each country in the world!
+        </p>
+      </section>
+      <section className="search">
+        <TextField
+          id="standard-basic"
+          label="Type a country, or a country’s capital"
+          variant="standard"
+          sx={textFieldStyling}
+          fullWidth
+        />
+        <button className="search-btn" onClick>
+          Search
+        </button>
+      </section>
+    </section>
+  );
 }
