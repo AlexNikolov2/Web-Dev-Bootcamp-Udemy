c490dad435689df02b8af5fdfd6c76c51c4a7fa6
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-11 01:34:25 +0300

    implmenting search logic

--- know-your-capital/front/src/pages/game/LearnMode/index.jsx
+++ know-your-capital/front/src/pages/game/LearnMode/index.jsx
@@ -1,15 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TextField } from "@mui/material";
 import { textFieldStyling } from "../../../utils/textfield_styling";
 import { Capital } from "./Capital";
+import { getCountries } from "../../../services/gameService";
 import "./style.css";
 
 export function LearnMode() {
   const [searched, setSearched] = useState(false);
   const [searchInput, setSearchInput] = useState("");
 
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState({});
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const countriesData = await getCountries();
+        setCountries(countriesData);
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+    fetchCountries();
+  }, []);
+
   const handleSearch = () => {
     if (searchInput.trim() !== "") {
+      const foundCountry = countries.find(
+        (country) =>
+          country.name.toLowerCase() === searchInput.toLowerCase() ||
+          country.capital.toLowerCase() === searchInput.toLowerCase()
+      );
+      if (foundCountry) {
+        setCountry(foundCountry);
+      } else {
+        alert("Country or capital not found.");
+      }
       setSearched(true);
     } else {
       alert("Please enter a valid country or capital name.");
@@ -40,6 +66,6 @@ export function LearnMode() {
       </section>
     </section>
   ) : (
-    <Capital />
+    <Capital country={country} />
   );
 }
