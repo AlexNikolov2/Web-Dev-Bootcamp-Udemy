bd8b13823509b27de2fad7ddd02b1aba6df6b0da
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-11 01:15:26 +0300

    implementing enpoint in the service

--- know-your-capital/front/src/services/gameService.js
+++ know-your-capital/front/src/services/gameService.js
@@ -1,25 +1,38 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/game';
+const API_URL = "http://localhost:5000/game";
 
 export const getCountries = async () => {
-    try {
-        const response = await axios.get(`${API_URL}/play-mode`);
-        console.log("Response data:", response.data);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching countries:", error);
-        throw error;
-    }
-}
+  try {
+    const response = await axios.get(`${API_URL}/play-mode`);
+    console.log("Response data:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching countries:", error);
+    throw error;
+  }
+};
 
 export const getCountryInfo = async (id) => {
-    try {
-        const response = await axios.get(`${API_URL}/play-mode/${id}`);
-        console.log("Response data:", response.data);
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching country information:", error);
-        throw error;
-    }
+  try {
+    const response = await axios.get(`${API_URL}/play-mode/${id}`);
+    console.log("Response data:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching country information:", error);
+    throw error;
+  }
+};
+
+export const searchCountry = async (query, id) => {
+  try {
+    const response = await axios.get(`${API_URL}/search/${id}`, {
+      params: { query },
+    });
+    console.log("Response data:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error searching for country or capital:", error);
+    throw error;
+  }
 };
