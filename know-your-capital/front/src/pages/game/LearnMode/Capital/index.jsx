385307b9dd78eb6a7830c8ab665442f5567780d0
Aleks Nikolov <tcr.official.world@gmail.com>
2025-04-12 00:33:49 +0300

    added back to search functionality

--- know-your-capital/front/src/pages/game/LearnMode/Capital/index.jsx
+++ know-your-capital/front/src/pages/game/LearnMode/Capital/index.jsx
@@ -1,5 +1,5 @@
 import "./style.css";
-export function Capital({ foundCountry }) {
+export function Capital({ foundCountry, searched }) {
   const {
     flag,
     capitalPopulation,
@@ -9,20 +9,27 @@ export function Capital({ foundCountry }) {
     capitalInfo,
   } = foundCountry;
 
+  const backToSearch = () => {
+    searched(false);
+  };
+
   return (
-    <section className="capital-wrap">
-      <section className="container-left">
-        <img src={flag} alt="" />
-        <p>Population : {capitalPopulation} </p>
-        <p>Founded in : {capitalYearFounded}</p>
-      </section>
-      <section className="container-right">
-        <section className="right-title">
-          <h2>{capital}</h2>
-          <h3>Capital of {country}</h3>
+    <>
+      <section className="capital-wrap">
+        <section className="container-left">
+          <img src={flag} alt="" />
+          <p>Population : {capitalPopulation} </p>
+          <p>Founded in : {capitalYearFounded}</p>
         </section>
-        <p>{capitalInfo}</p>
+        <section className="container-right">
+          <section className="right-title">
+            <h2>{capital}</h2>
+            <h3>Capital of {country}</h3>
+          </section>
+          <p>{capitalInfo}</p>
+        </section>
       </section>
-    </section>
+      <button onClick={backToSearch}>Go Back</button>
+    </>
   );
 }
