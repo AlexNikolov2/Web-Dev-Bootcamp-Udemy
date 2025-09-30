f7d70fdfbcfa46252788524e50482559bb2f35f1
AlexNikolov2 <aletoooking00@gmail.com>
2025-09-30 22:53:46 +0300

    setting up modals

--- know-your-capital/front/src/pages/user/EditUser/index.jsx
+++ know-your-capital/front/src/pages/user/EditUser/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useAuth } from "../../../contexts/AuthContext";
-import { Input } from "@mui/material";
+import { Input, Modal } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
 import { editUser } from "../../../services/userService";
@@ -13,12 +13,18 @@ export const EditUser = () => {
   const [image, setImage] = useState("");
   const [setError] = useState(null);
 
+  const [open, setOpen] = useState(true);
+  const handleClose = () => {
+    setOpen(false);
+    navigate(`/user/${user._id}`);
+  };
+
   console.log({ username, email, image });
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
       await editUser({ email, username, image }, user._id);
+      handleClose();
       navigate(`/user/${user._id}`);
     } catch (error) {
       console.error("Edit user failed:", error);
@@ -27,34 +33,36 @@ export const EditUser = () => {
   };
 
   return (
-    <section className="edit-user-wrap">
-      <h2>Edit Profile</h2>
-      <form onSubmit={handleSubmit}>
-        <Input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          fullWidth
-          autoComplete="username"
-        />
-        <Input
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          type="email"
-          fullWidth
-          autoComplete="email"
-        />
-        <Input
-          onChange={(e) => setImage(e.target.files[0])}
-          placeholder="Profile Image"
-          type="file"
-          fullWidth
-        />
-        <button type="submit" onClick={handleSubmit}>
-          Save Changes
-        </button>
-      </form>
-    </section>
+    <Modal open={open} onClose={handleClose}>
+      <section className="edit-user-wrap">
+        <h2>Edit Profile</h2>
+        <form onSubmit={handleSubmit}>
+          <Input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            fullWidth
+            autoComplete="username"
+          />
+          <Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            type="email"
+            fullWidth
+            autoComplete="email"
+          />
+          <Input
+            onChange={(e) => setImage(e.target.files[0])}
+            placeholder="Profile Image"
+            type="file"
+            fullWidth
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Save Changes
+          </button>
+        </form>
+      </section>
+    </Modal>
   );
 };
