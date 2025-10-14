fe23362ab8ed5f9f86f557b9df501d0afa06bb71
AlexNikolov2 <aletoooking00@gmail.com>
2025-10-14 23:46:50 +0300

    created game service

--- know-your-capital/back/services/gameService.js
+++ know-your-capital/back/services/gameService.js
@@ -1,1 +1,19 @@
-const 
+import Game from "../models/Game";
+
+const gameService = {
+  saveCurrentGame: async (gameData) => {
+    const game = new Game(gameData);
+    await game.save();
+    return game;
+  },
+
+  getAllGames: async () => {
+    return await Game.find();
+  },
+
+  getLatestGame: async () => {
+    return await Game.findOne().sort({ createdAt: -1 });
+  },
+};
+
+export default gameService;
