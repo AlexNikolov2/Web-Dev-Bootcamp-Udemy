82192cad9c313f9e38f75ad29df0f43d94998293
AlexNikolov2 <aletoooking00@gmail.com>
2025-10-23 12:47:03 +0300

    added gameId parameter

--- know-your-capital/back/models/Game.js
+++ know-your-capital/back/models/Game.js
@@ -1,23 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const gameSchema = mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    score: {
-        type: Number,
-        default: 0,
-    },
-    timeTaken: {
-        type: Number,
-        default: 0,
-    },
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+  timeTaken: {
+    type: Number,
+    default: 0,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Game = mongoose.model("Game", gameSchema);
