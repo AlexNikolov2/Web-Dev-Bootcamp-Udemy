bf93115ea1f8ad88cb2c7e13d87f7e099ff4ae7a
Aleks Nikolov <tcr.official.world@gmail.com>
2025-11-01 01:10:23 +0200

    added navigation to the game ending component

--- know-your-capital/front/src/pages/game/PlayMode/GameCompletion/index.jsx
+++ know-your-capital/front/src/pages/game/PlayMode/GameCompletion/index.jsx
@@ -1,20 +1,44 @@
+import { formatTime } from "../Timer/utils";
+import { useNavigate } from "react-router";
+
 export function GameCompletion({
   correctCountries,
   totalCountries,
   timeTaken,
 }) {
+  const navigate = useNavigate();
+
+  const handleTryAgain = () => {
+    navigate("/games/play-mode");
+  };
+
+  const handleGoToMainMenu = () => {
+    navigate("/");
+  };
+
   return (
     <section className="game-completion">
       {correctCountries === totalCountries ? (
         <>
           <h2>Congratulations! You&apos;ve matched all the capitals!</h2>
-          <span>Time taken: {timeTaken}</span>
+          <span>Time taken: {formatTime(timeTaken * 1000)}</span>
+          <section className="buttons">
+            <button onClick={handleTryAgain}>Try Again</button>
+            <button onClick={handleGoToMainMenu}>Go to Main Menu</button>
+          </section>
         </>
       ) : (
-        <h2>
-          Game Over! You answered {correctCountries} out of {totalCountries}{" "}
-          countries countries correctly.
-        </h2>
+        <>
+          <h2>
+            Game Over! You answered {correctCountries} out of {totalCountries}{" "}
+            countries correctly.
+          </h2>
+          <span>Time taken: {formatTime(timeTaken * 1000)}</span>
+          <section className="buttons">
+            <button onClick={handleTryAgain}>Try Again</button>
+            <button onClick={handleGoToMainMenu}>Go to Main Menu</button>
+          </section>
+        </>
       )}
     </section>
   );
