c5f768d570ba707e373bdf046fbc479aa409fb95
AlexNikolov2 <aletoooking00@gmail.com>
2025-11-06 08:54:14 +0200

    inits

--- know-your-capital/front/src/pages/game/TopGames/index.jsx
+++ know-your-capital/front/src/pages/game/TopGames/index.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export const TopGames = () => {
+  const [topGames, setTopGames] = useState([]);
+
   return (
     <section className="top-games">
       <h2>Top Games</h2>
